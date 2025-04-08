b353b0f0b7e82ed7a9146e783cd22bf9847abede
felipecardosocosta <felipe_cardoso_costa@estudante.sc.senai.br>
2025-04-08 20:37:25 -0300

    Adicionei exercicios novos em js aula prof alex

---
+++ Prof. Alex/js/aula.js
@@ -0,0 +1,21 @@
+/*let idade = prompt('Digite sua idade')
+if(idade>=18){
+    console.log('voce é maior de idade')
+}else{
+    console.log('voce é menor de idade ')
+
+}*/
+
+let numero1 = Number(prompt('digite uma nota : '))
+let numero2 = Number(prompt('digite outra nota: '))
+let numero3 = Number(prompt('digite outra nota: '))
+let numero4 = Number(prompt('digite outra nota: '))
+let media
+
+media = (numero1 + numero2 + numero3 + numero4)/4
+
+console.log('a media é ', media)
+
+let elementoH1  = document.getelementid("resultado")
+
+
