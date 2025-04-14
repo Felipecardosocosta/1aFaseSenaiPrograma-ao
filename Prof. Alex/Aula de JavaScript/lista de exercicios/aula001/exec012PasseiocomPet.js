f83e69ecab54b8df61e1df84ba9dd3008707391f
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-04-13 23:17:04 -0300

    Adicionei novos exercicios

---
+++ Prof. Alex/Aula de JavaScript/lista de exercicios/aula001/exec012PasseiocomPet.js
@@ -0,0 +1,42 @@
+/*//14. Passeio com o Pet
+        Lista de exercícios 6
+        História:
+        Você só vai passear com seu cachorro se não estiver chovendo e se tiver tempo.
+        Desafio:
+        let estaChovendo;
+        let temTempo;
+        Exiba:
+        "Hora do passeio! 🐾" ou
+        "Hoje não vai dar... ☔" */
+
+
+let estaChovendo
+let temTempo
+let chovendo
+let semTempo
+
+
+
+estaChovendo = confirm("Esta chovendo ? ")
+
+if (estaChovendo === false) {
+
+        temTempo = confirm("Voce esta com tempo?")
+
+        if (temTempo === true) {
+                alert("Hora do passeio! 🐾")
+        } else {
+                alert("Hoje não vai dar... ☔")
+        }
+
+} else alert("Hoje não vai dar... ☔")
+
+
+
+
+
+
+
+
+
+
