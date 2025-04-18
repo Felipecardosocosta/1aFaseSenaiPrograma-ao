0a47b1f90b2c302ff5517beced79a68d27e03a43
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-04-18 01:32:09 -0300

    adicionei melhorias

--- aula Prof.Rafael/Eco moeda/Ecomoeda.js
+++ aula Prof.Rafael/Eco moeda/Ecomoeda.js
@@ -1,19 +1,43 @@
 //const qtdBrinq = Number(document.getElementById("nuBrinq"))
 
+let banco = [
+    ["nome"],
+    ["id"],
+    ["Ecomoeda"]
+]
+
 
 function enviou() {
     const qtdBrinq = Number(document.getElementById("nuBrinq").value)
     const result = document.getElementById("Ecomoeda")
-    console.log(qtdBrinq)
+    let Ecomoed = 0
+    banco[0].push(prompt("Digite seu nome"))
+    banco[1].push(Number(prompt("Digite seu id")))
+    console.log(banco)
+
+
     if (qtdBrinq === 0) {
+
         result.innerHTML = `Voce nao ganhou nada!`
 
     } else if (qtdBrinq === 1) {
-        result.innerHTML = `Voce ganhou uma moeda!`
+        Ecomoed += 1
+        console.log(Ecomoed)
+        banco[2].push(Ecomoed)
+
+        result.innerHTML = `Voce ganhou ${Ecomoed} Ecomoeda`
+
     } else if (qtdBrinq === 2) {
-        result.innerHTML = `Voce ganhou duas moedas!`
+        Ecomoed += 2
+        banco[2].push(Ecomoed)
+
+        result.innerHTML = `Voce ganhou ${Ecomoed} Ecomoeda`
     } else if (qtdBrinq >= 3) {
-        result.innerHTML = `Voce ganhou Tres!`
+        Ecomoed += 3
+        banco[2].push(Ecomoed)
+        result.innerHTML = `Voce ganhou ${Ecomoed} Ecomoeda`
     }
+
+    console.log(banco)
 
 }
