0cf8deff0641fe9aad9c7b3df02ee4ec7f44f623
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-04-21 17:48:55 -0300

    Adicionei exercicios novos

--- desafio da internet/TioGuanabara/Laço de repetição/exemplo1.js
+++ desafio da internet/TioGuanabara/Laço de repetição/exemplo1.js
@@ -1,17 +1,21 @@
-let n = 0
+// let n = 0
 
 
 
-while (n <= 8) {
-    console.log(n)
-    n++
+// while (n <= 8) {
+//     console.log(n)
+//     n++
+// }
+
+// n = 0
+
+// do {
+//     console.log(n)
+//     n++
+
+
+// } while (n <= 10)
+
+for (let n = 1; n <= 10; n++) {
+    console.log(`Oieeee ${n}`)
 }
-
-n = 0
-
-do {
-    console.log(n)
-    n++
-
-
-} while (n <= 10)
---
+++ desafio da internet/TioGuanabara/Laço de repetição/modelo/1Jsmodelo.js
@@ -0,0 +1,26 @@
+
+
+
+function enviar() {
+    let inicio = Number(document.getElementById
+        ("inicio").value)
+    let fim = Number(document.getElementById("fim").value)
+
+    let passo = Number(document.getElementById("passo").value)
+    let resultado = document.getElementById("resultado")
+
+    if(inicio ===0 || fim ===0 || passo ===0){
+        alert("[ERRO!!] Preencha tudo para poder contar")
+
+    }else {
+        resultado.innerHTML = `Contando:<br>`
+
+    for (let variavel = inicio; variavel < fim; variavel += passo) {
+
+        resultado.innerHTML += ` ${variavel}`
+        console.log(variavel)
+    }
+
+    }
+    
+}
