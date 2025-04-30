0efe714816a2ba16e3b9c713bbf27d8469a17289
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-04-30 01:37:19 -0300

    Adicionei exercicios novos

--- Prof. Alex/Aula de JavaScript/lista de exercicios/aula003/lista de exercicios/exerc007/exerc007.js
+++ Prof. Alex/Aula de JavaScript/lista de exercicios/aula003/lista de exercicios/exerc007/exerc007.js
@@ -11,13 +11,18 @@ function gerar() {
     let quandidade = Number(document.getElementById("QtdDigito").value)
 
     let senha = ""
+    if (quandidade == "") {
+        alert("[ERRO] Digite a quandidade de caracteres")
+    } else {
 
-    for (let controle = 0; controle <= quandidade; controle++) {
 
-        senha += gerandoCaracteres()
+        for (let controle = 1; controle <= quandidade; controle++) {
 
+            senha += gerandoCaracteres()
+
+        }
+
+        document.getElementById("resultado").innerHTML = `<p>Sua senha é : <br> <br><strong>${senha}</strong> </p>`
     }
-
-    document.getElementById("resultado").innerHTML = `<p>Sua senha é : <br>${senha} </p>`
 
 }
