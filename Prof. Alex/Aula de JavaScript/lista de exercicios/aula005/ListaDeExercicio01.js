ffcd4714e44fbe7dda91d0519d9506987cb83004
Felipecardosodacosta <felipe.cardoso.costa96@gmail.com>
2025-04-29 22:10:23 -0300

    Adicionei exercicios novos

---
+++ Prof. Alex/Aula de JavaScript/lista de exercicios/aula005/ListaDeExercicio01.js
@@ -0,0 +1,49 @@
+function mouseEntrou1() {
+    document.querySelector("#exerc001").classList.add("divMovimenta")
+}
+function mouseSaiu1() {
+    document.querySelector("#exerc001").classList.remove("divMovimenta")
+}
+function mouseEntrou2(){
+    document.querySelector("#exerc002").classList.add("divMovimenta")
+
+}
+function mouseSaiu2(){
+    document.querySelector("#exerc002").classList.remove("divMovimenta")
+}
+function mouseEntrou3(){
+    document.querySelector("#exerc003").classList.add("divMovimenta")
+}
+function mouseSaiu3(){
+    document.querySelector("#exerc003").classList.remove("divMovimenta")
+}
+function mouseEntrou4(){
+    document.querySelector("#exerc004").classList.add("divMovimenta")
+}
+function mouseSaiu4(){
+    document.querySelector("#exerc004").classList.remove("divMovimenta")
+}
+function mouseEntrou5(){
+    document.querySelector(`#exerc005`).classList.add("divMovimenta")
+}
+function mouseSaiu5(){
+    document.querySelector(`#exerc005`).classList.remove("divMovimenta")
+}
+function mouseEntrou6(){
+    document.querySelector("#exerc006").classList.add("divMovimenta")
+}
+function mouseSaiu6(){
+    document.querySelector("#exerc006").classList.remove("divMovimenta")
+}
+function mouseEntrou7(){
+    document.querySelector("#exerc007").classList.add("divMovimenta")
+}
+function mouseSaiu7(){
+    document.querySelector("#exerc007").classList.remove("divMovimenta")
+}
+function mouseEntrou8(){
+    document.querySelector("#exerc008").classList.add("divMovimenta")
+}
+function mouseSaiu8(){
+    document.querySelector("#exerc008").classList.remove("divMovimenta")
+}
