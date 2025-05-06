b6765de1233512682a5d1422eff28f472847345c
Felipecardosodacosta <felipe.cardoso.costa96@Gmail.com>
2025-05-06 20:47:01 -0300

    Adicionei exercicios novos

--- Prof. Alex/Aula de JavaScript/lista de exercicios/aula004/lista de exercicios/exerc002/exerc002.js
+++ Prof. Alex/Aula de JavaScript/lista de exercicios/aula004/lista de exercicios/exerc002/exerc002.js
@@ -2,15 +2,12 @@ let criarNumeroAleatorio = (n) => {
     
     let numero = Math.round(Math.random()*(n-1)+1)
 
-
-    if (numero ==2) {
-        criarNumeroAleatorio(n)
-        
-    }else{
     return numero
-    }
+    
     
 }
 
-let n = criarNumeroAleatorio(5)
-console.log(n)
+let test = criarNumeroAleatorio(5)
+console.log(test)
+
+
