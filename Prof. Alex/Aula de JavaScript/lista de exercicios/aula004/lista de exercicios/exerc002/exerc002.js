69985f231c714ee154085bbadf1e5ce0576c6a83
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-05-12 17:51:52 -0300

    Adicionei exercicios

--- Prof. Alex/Aula de JavaScript/lista de exercicios/aula004/lista de exercicios/exerc002/exerc002.js
+++ Prof. Alex/Aula de JavaScript/lista de exercicios/aula004/lista de exercicios/exerc002/exerc002.js
@@ -1,13 +1,60 @@
 let criarNumeroAleatorio = (n) => {
-    
-    let numero = Math.round(Math.random()*(n-1)+1)
+
+    let numero = Math.round(Math.random() * (n - 1) + 1)
 
     return numero
-    
-    
+
+
 }
 
 let test = criarNumeroAleatorio(5)
 console.log(test)
+
+
+
+let array = [{
+    nome: "felipe",
+    idade: 29,
+    saudaçao(n=0, i) {
+        
+        console.log(`Ola eu sou o ${this.nome} e tenho ${this.idade}`)
+
+        for(let cont=0  ;cont<=i  ;cont++){
+            n = this.idade + cont
+            console.log(`Agora minha idade é ${n}`)
+            
+        }
+        this.idade = n
+    }
+},{
+    nome: `Dara`,
+    idade: 18
+
+}
+
+
+]
+
+
+
+
+array[0].saudaçao(1,5)
+array.push({
+    nome: `oie`,
+    idade: 5
+    ,amor: `eu`
+    })
+array.forEach(amor=> amor.amor = `felipe`)
+let descobrindo = array.filter(estilo => estilo.amor === 'felipe').map(d=> d.nome) //.reduce(oie=> oie.amor =="felipe")
+
+// let naosei = array.fill(amorr=> amorr.amor = `euuuuu`)
+// console.log(naosei);
+
+
+console.log(descobrindo)
+
+console.log(array);
+
+
 
 
