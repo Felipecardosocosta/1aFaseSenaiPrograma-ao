e3d4118623415d892bbe381c77e10fd17261c364
Felipe Cardosocosta <felipe.cardoso.costa96@gmail.com>
2025-07-08 18:48:10 -0300

    adc

--- Prof. Alex/exercicio para entregar/localStorage/exerc10/exerc.js
+++ Prof. Alex/exercicio para entregar/localStorage/exerc10/exerc.js
@@ -5,3 +5,42 @@
 // acertar.
 
 
+class NumeroSecreto{
+
+constructor(){
+    this.numeroSecrt = this.pesquisa()
+
+}
+gerandoNumero(){
+    let min = 1
+    let max = 10
+
+    return Math.round(Math.random()*(max-min)+min)
+}
+recebendoNumero(){
+    return Number(document.getElementById("").value)||false
+}
+comparandoNumero(){
+
+    let numero = this.recebendoNumero()
+
+    return numero===this.numeroSecrt ? numero: false
+}
+salvandoNumero(){
+    localStorage.setItem("NumeroSecret",JSON.stringify(this.numeroSecrt))
+}
+pesquisa(){
+    return JSON.parse(localStorage.getItem("NumeroSecret"))
+}
+AnalizandoDados(){
+
+    if (condition) {
+        
+    }
+
+}
+
+
+}
+let novoJogo = new NumeroSecreto
+console.log(novoJogo.gerandoNumero());
